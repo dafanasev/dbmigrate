03eb2aea74ac27c72de57f551aa5fbb3c23fa973
Dmitrii Afanasev <nuclear.tools@gmail.com>
2018-10-04 00:08:53 +0400

    redoCmd tests/refactor

--- cmd/redo_cmd.go
+++ cmd/redo_cmd.go
@@ -17,50 +17,54 @@ var redoCmd = &cobra.Command{
 	Use:   "redo",
 	Short: "redo last batch",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		done := make(chan struct{})
-		gdone := make(chan struct{})
-
-		go func() {
-			for {
-				select {
-				case err := <-migrator.ErrorsCh:
-					fmt.Fprintln(os.Stderr, errors.Wrap(err, "migration error"))
-				case migration := <-migrator.MigrationsCh:
-					switch migration.Direction {
-					case dbmigrate.DirectionUp:
-						fmt.Printf("migration %s has been successfully applied\n", migration.FileName())
-					case dbmigrate.DirectionDown:
-						fmt.Printf("migration %s has been successfully rolled back\n", migration.FileName())
-					}
-				case <-done:
-					close(gdone)
-					return
-				}
-			}
-		}()
-
-		var n int
-		n, err := migrator.Rollback()
-		if err != nil {
-			close(done)
-			return errors.Wrap(err, "can't redo: can't rollback")
-		}
-		if n == 0 {
-			close(done)
-			<-gdone
-			fmt.Println("there are no migrations to reapply")
-			return nil
-		}
-
-		n, err = migrator.MigrateSteps(n)
-		close(done)
-
-		<-gdone
-		if err != nil {
-			return errors.Wrap(err, "can't redo: can't migrate")
-		}
-		fmt.Printf("%d %s successfully reapplied\n", n, pluralize("migration", n))
-
-		return nil
+		_, err := redo(migrator, steps)
+		return err
 	},
+}
+
+func redo(migrator *dbmigrate.Migrator, steps int) (int, error) {
+	done := make(chan struct{})
+	gdone := make(chan struct{})
+
+	go func() {
+		for {
+			select {
+			case err := <-migrator.ErrorsCh:
+				fmt.Fprintln(os.Stderr, errors.Wrap(err, "migration error"))
+			case migration := <-migrator.MigrationsCh:
+				switch migration.Direction {
+				case dbmigrate.DirectionUp:
+					fmt.Printf("migration %s has been successfully applied\n", migration.FileName())
+				case dbmigrate.DirectionDown:
+					fmt.Printf("migration %s has been successfully rolled back\n", migration.FileName())
+				}
+			case <-done:
+				close(gdone)
+				return
+			}
+		}
+	}()
+
+	n, err := migrator.RollbackSteps(steps)
+	if err != nil {
+		close(done)
+		return n, errors.Wrap(err, "can't redo: can't rollback")
+	}
+	if n == 0 {
+		close(done)
+		<-gdone
+		fmt.Println("there are no migrations to reapply")
+		return n, nil
+	}
+
+	n, err = migrator.MigrateSteps(n)
+	close(done)
+
+	<-gdone
+	if err != nil {
+		return n, errors.Wrap(err, "can't redo: can't migrate")
+	}
+	fmt.Printf("%d %s successfully reapplied\n", n, pluralize("migration", n))
+
+	return n, nil
 }
---
+++ cmd/redo_cmd_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/dafanasev/dbmigrate"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_redo(t *testing.T) {
+	createTestMigrations()
+	defer os.RemoveAll(dbmigrate.MigrationsDir)
+	defer os.Remove("test.db")
+
+	migrator, _ := dbmigrate.NewMigrator(&dbmigrate.Settings{
+		Engine: "sqlite", Database: "test.db",
+		MigrationsCh: make(chan *dbmigrate.Migration), ErrorsCh: make(chan error),
+	})
+	defer migrator.Close()
+
+	n, err := redo(migrator, 0)
+	require.NoError(t, err)
+	assert.Equal(t, 0, n)
+
+	migrate(migrator, dbmigrate.AllSteps)
+	n, err = redo(migrator, 0)
+	require.NoError(t, err)
+	assert.Equal(t, 3, n)
+
+	migrate(migrator, dbmigrate.AllSteps)
+	n, err = redo(migrator, 2)
+	require.NoError(t, err)
+	assert.Equal(t, 2, n)
+
+	os.Rename(filepath.Join(dbmigrate.MigrationsDir, "20180918200453.first.down.sql"), "./20180918200453.first.down.sql")
+	n, err = redo(migrator, 0)
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), "can't rollback")
+	assert.Equal(t, 0, n)
+
+	migrate(migrator, dbmigrate.AllSteps)
+	migrator.AllowMissingDowns = true
+	n, err = redo(migrator, 0)
+	require.NoError(t, err)
+	assert.Equal(t, 2, n)
+	os.Rename("./20180918200453.first.down.sql", filepath.Join(dbmigrate.MigrationsDir, "20180918200453.first.down.sql"))
+}
